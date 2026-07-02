c51259b7f02df639847d61405df6044c9af86ccc
fon.suteara <fon.suteara@gmail.com>
2026-07-02 13:36:31 +0700

    my-object.js

---
+++ my-array.js
@@ -0,0 +1,5 @@
+let food = ["Pizza", "PadThai", "Pasta"];
+
+console.log(food[0]);
+console.log(food[1]);
+console.log(food[2]);
---
+++ my-object.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Object</title>
+</head>
+<body>
+    <h1>Food Database</h1>
+    <ul>
+        <li>Pizza</li>
+        <li>PadThai</li>
+        <li>Pasta</li>
+    </ul>
+</body>
+</html>
