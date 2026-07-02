45f77900e5cdf175dabf956732124623f20b5d51
fon.suteara <fon.suteara@gmail.com>
2026-07-02 13:45:34 +0700

    addning data in array and object

--- my-object.js
+++ my-object.js
@@ -1,16 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Object</title>
-</head>
-<body>
-    <h1>Food Database</h1>
-    <ul>
-        <li>Pizza</li>
-        <li>PadThai</li>
-        <li>Pasta</li>
-    </ul>
-</body>
-</html>
+let foodDatabase = [
+    {
+        id: 1,
+        name: "Pizza",
+        size: "small",
+        cuisine: "Italian", 
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 2,
+        name: "PadThai",
+        size: "small",
+        cuisine: "Italian", 
+        origin: "Italy",
+        vegetarian: false,
+    },
+    {
+        id: 3,
+        name: "Pasta",
+        size: "small",
+        cuisine : "Italian", 
+        origin: "Italy",
+        vegetarian: false,
+    },
+];
+
+console.log(foodDatabase[0].name);
+
+//console.log(foodDatabase[1].name);
+
+//console.log(foodDatabase[2].name);
